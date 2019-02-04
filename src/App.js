7b2439d058896553c3a23b10548a8554580797c2
Enzo <enzo@localhost.localdomain>
2019-02-04 19:17:46 +0100

    add some routing

--- src/App.js
+++ src/App.js
@@ -1,54 +1,17 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
-import Axios from 'axios';
-import LaunchesListView from './components/LaunchesListView';
+import LaunchSingleView from './components/LaunchSingleView';
+import Homepage from './components/Homepage';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      companyInfo: {},
-      companyTwitter: '',
-      companyWebsite: ''
-    };
-  }
-
-  async componentDidMount() {
-    const { data: companyInfo } = await Axios.get(
-      'https://api.spacexdata.com/v3/info'
-    );
-    const companyTwitter = companyInfo.links.twitter;
-    const companyWebsite = companyInfo.links.website;
-    this.setState({ companyInfo, companyTwitter, companyWebsite });
-  }
-
   render() {
     return (
       <React.Fragment>
-        <section className="hero is-fullheight spacex-bg">
-          <div className="hero-body">
-            <div className="container has-text-centered hero-content">
-              <h1 className="title is-1 has-text-white">SpaceX Fan</h1>
-              <p className="subtitle has-text-white">
-                {this.state.companyInfo.summary}
-              </p>
-              <span className="icon">
-                <a href={this.state.companyTwitter}>
-                  <i className="fa fa-twitter fa-lg fa-2x" aria-hidden="true" />
-                </a>
-              </span>
-              <span className="icon">
-                <a href={this.state.companyWebsite}>
-                  <i
-                    class="fa fa-space-shuttle fa-lg fa-2x"
-                    aria-hidden="true"
-                  />
-                </a>
-              </span>
-            </div>
-          </div>
-        </section>
-        <LaunchesListView />
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route exact path="/launch/:id" component={LaunchSingleView} />
+        </Switch>
       </React.Fragment>
     );
   }
--- src/components/LaunchListView/index.jsx
+++ src/components/LaunchListView/index.jsx
@@ -1,26 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './style.css';
 
 function LaunchListView({ launch }) {
   return (
     <div className="column is-one-quarter-fullhd is-one-third-desktop is-half-tablet is-one-mobile">
-      <div className="card">
-        <div className="card-header">
-          <figure className="image has-text-centered">
-            <img
-              className="card-mission-patch inline-block"
-              src={launch.links.mission_patch_small}
-              alt="mission patch"
-            />
-          </figure>
-        </div>
-        <div className="card-content">
-          <div className="content">
-            <p className="title is-4">{launch.mission_name}</p>
-            <p className="subtitle is-6">{launch.rocket.rocket_name}</p>
+      <Link to={`/launch/${launch.flight_number}`}>
+        <div className="card">
+          <div className="card-header">
+            <figure className="image has-text-centered">
+              <img
+                className="card-mission-patch inline-block"
+                src={launch.links.mission_patch_small}
+                alt="mission patch"
+              />
+            </figure>
+          </div>
+          <div className="card-content">
+            <div className="content">
+              <p className="title is-4">{launch.mission_name}</p>
+              <p className="subtitle is-6">{launch.rocket.rocket_name}</p>
+            </div>
           </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 }
